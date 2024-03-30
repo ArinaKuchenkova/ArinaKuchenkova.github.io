002223fc6afd47292b2f575e1cb8cf56e6e0a139
deadrime <deadrime@yandex.ru>
2024-03-30 23:42:55 +0300

    Update WidthInfo component

---
+++ components/WidthInfo/WidthInfo.tsx
@@ -0,0 +1,18 @@
+"use client";
+import useMeasure from "react-use-measure";
+import Typography from "@/components/Typography";
+import styles from './WidthInfo.module.css';
+import { cn } from "@/lib/utils";
+import { ComponentPropsWithoutRef } from "react";
+
+export const WidthInfo: React.FC<ComponentPropsWithoutRef<'div'>> = ({ className }) => {
+  const [ref, { width }] = useMeasure()
+  return (
+    <div ref={ref} className={cn("self-center", className, styles.widthInfo)}>
+      <div className={styles.widthInfoLine} />
+      <Typography className="whitespace-nowrap absolute left-1/2 bottom-3 -translate-x-1/2 text-peach">
+        {Math.round(width)} px
+      </Typography>
+    </div>
+  )
+}
