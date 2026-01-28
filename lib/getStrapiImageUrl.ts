36abda6ef67909377a0bafaa22ff8b0d629c9d77
Eugene <deadrime@yandex.ru>
2026-01-28 20:54:52 +0300

    fix: correct replace

--- lib/getStrapiImageUrl.ts
+++ lib/getStrapiImageUrl.ts
@@ -1,5 +1,20 @@
 import { NEXT_PUBLIC_IMAGES_PREFIX, NEXT_PUBLIC_STRAPI_URL } from "./env"
 
+function extractSubdomainPath(url: string): string | null {
+  const regex = /^https?:\/\/([a-z0-9-]+)\./i;
+  const match = url.match(regex);
+
+  if (!match) {
+    return null;
+  }
+
+  const subdomain = match[1];
+  return subdomain;
+}
+
+const strapiProject = extractSubdomainPath(NEXT_PUBLIC_STRAPI_URL);
+const strapiMediaUrl = `https://${strapiProject}.media.strapiapp.com`;
+
 export const getStrapiImageUrl = (url?: string) => {
   if (!url) {
     return ''
@@ -7,8 +22,8 @@ export const getStrapiImageUrl = (url?: string) => {
   if (url?.startsWith('/')) {
     return `${NEXT_PUBLIC_IMAGES_PREFIX}${url}`
   }
-  if (url?.startsWith(NEXT_PUBLIC_STRAPI_URL)) {
-    return url.replace(NEXT_PUBLIC_STRAPI_URL, '/');
+  if (url?.startsWith(strapiMediaUrl)) {
+    return url.replace(strapiMediaUrl, '/uploads');
   }
   return url
 }
